9fb7d07d6b8f979c1059b133c1f2aabab4caa61d
= <louist87@gmail.com>
2012-09-10 08:34:30 +0200

    Tweaked setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(
-    name='tromegle',
-    version='0.1.0',
+    name='Tromegle',
+    version='0.1.0 alpha',
     author='Louis Thibault',
-    author_email='???',
+    author_email='',
     packages=['tromegle'],
     include_package_data=True,
     url='https://github.com/louist87/tromegle',
-    license='???',
-    description='troll strangers!',
+    license='Creative Commons BY-NC-SA 3.0',
+    description='Troll strangers!',
     long_description=open('README.md').read()
 )
