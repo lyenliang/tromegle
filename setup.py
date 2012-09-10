1bd4a5c20009b72bf059945fdc48c5681422a3b2
= <louist87@gmail.com>
2012-09-10 20:38:57 +0200

    Changed license to GPL 3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     install_requires=['Twisted>=11.1.0'],
     url='https://github.com/louist87/tromegle',
-    license='Creative Commons BY-NC-SA 3.0',
+    license='GPL 3.0',
     description='Troll strangers!',
     long_description=open('README.md').read()
 )
