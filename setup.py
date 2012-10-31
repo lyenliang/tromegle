3024cff1cbb934640c71e4d96d5124b4699fad83
Louis Thibault <louist@ltpc.(none)>
2012-10-31 15:54:19 +0100

    incremented version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tromegle',
-    version='0.2.0 alpha',
+    version='0.2.1 alpha',
     author='Louis Thibault',
     author_email='',
     packages=['tromegle'],
