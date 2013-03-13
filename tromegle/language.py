bd55767fc8d1948b8a5307d24ef321424cecd99d
Louis Thibault <louist@ltpc.(none)>
2013-03-13 06:53:39 +0100

    Major updates.  Tokenizer and SubstitutionMap added

--- tromegle/language.py
+++ tromegle/language.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
-from string import punctuation, whitespace
-from itertools import groupby
+# from string import punctuation, whitespace
+# from itertools import groupby, tee, izip
+import re
+from functools import wraps
+import Levenshtein as strndist
+from unicodedata import normalize as uninorm
+
+
+def normalize_unicode(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        args_normed = []
+        for a in args:
+            if isinstance(a, unicode):
+                a = uninorm('NFD', a)
+            args_normed.append(a)
+
+        kwargs_normed = {}
+        for k, v in kwargs.items():
+            if isinstance(k, unicode):
+                k = uninorm(a, 'NFD')
+            if isinstance(v, unicode):
+                v = uninorm(v, 'NFD')
+            kwargs_normed[k] = v
+        return func(*args, **kwargs)
+    return wrapper
 
 
 def mapCapitals(phrase):
@@ -44,24 +68,80 @@ def fuzzyCaps(n_token, o_token):
     return n_token.lower()
 
 
-def tokenize(phrase, sepcat=True):
-    """Parse a string into words (strings separated by whitespace.
+class Tokenizer(object):
+    _other = '\s+|\w+|[^\s\w]+'
 
-    All punctuation characters and typographical symbols are treated as
-    an individual word.
+    def __init__(self, stop_words=set(), case_sensitive=False):
+        self.case_sensitive = case_sensitive
 
-    phrase : str
-        Phrase to serparate into words.
+        self.stop_words = stop_words
+        stop_words = '|'.join(re.escape(x) + r'\b' for x in stop_words)
+        self.token_regex = stop_words + '|' + self._other if stop_words else self._other
 
-    sepcat : bool
-        Separator concatination.
-        If true, adjacent separators will be concatenated into a single token.
+    def __call__(self, phrase, case_sensitive_override=None):
+        case_sensitive = case_sensitive_override if (case_sensitive_override is not None) else self.case_sensitive
+        flag = 0 if case_sensitive else re.IGNORECASE
 
-    return : list
-        List of words.
-    """
-    separators = dict.fromkeys(whitespace + punctuation, True)
-    return [''.join(g) for _, g in groupby(phrase, separators.get)]
+        return re.findall(self.token_regex, phrase, flag)
+
+
+# def tokenize(phrase, stop_words=set()):
+#     """Parse a string into words (strings separated by whitespace.
+
+#     All punctuation characters and typographical symbols are treated as
+#     an individual word.
+
+#     phrase : str
+#         Phrase to serparate into words.
+
+#     return : list
+#         List of words.
+#     """
+#     stop_words = '|'.join(re.escape(x) + r'\b' for x in stop_words)
+#     other = '\s+|\w+|[^\s\w]+'
+#     regex = stop_words + '|' + other if stop_words else other
+#     return re.findall(regex, phrase, re.IGNORECASE)
+
+
+# #   NEED TO OPTIMIZE:  looks like exponential time
+# def tokenize(phrase, stop_words=None):
+#     """Parse a string into words (strings separated by whitespace.
+
+#     All punctuation characters and typographical symbols are treated as
+#     an individual word.
+
+#     phrase : str
+#         Phrase to serparate into words.
+
+#     return : list
+#         List of words.
+#     """
+#     separators = dict.fromkeys(whitespace + punctuation, True)
+#     tokens = [''.join(g) for _, g in groupby(phrase, separators.get)]
+
+#     if stop_words:
+#         assert isinstance(stop_words, set), 'stop_words must be a set'
+#         window = 2  # Iterating over single tokens is useless
+#         while window <= len(tokens):
+#             # "sliding window" over token list
+#             iters = tee(tokens, window)
+#             for i, offset in izip(iters, xrange(window)):
+#                 for _ in xrange(offset):
+#                     next(i, None)
+
+#             # Join each window and check if it's in `stop_words`
+#             for offset, tkgrp in enumerate(izip(*iters)):
+#                 tk = ''.join(tkgrp)
+#                 if tk in stop_words:
+#                     pre = tokens[0: offset]
+#                     post = tokens[offset + window + 1::]
+#                     tokens = pre + [tk] + post
+#                     window = 1  # will be incremented after breaking from loop
+#                     break
+
+#             window += 1
+
+#     return tokens
 
 
 def isSymbol(char):
@@ -77,62 +157,86 @@ def isSymbol(char):
     return not char.isalpha and not char.isdigit and not char.isspace
 
 
-def levenshtein_dist(s1, s2):
+class SubstitutionMap(dict):
+    """String replacement with fuzzy matching and formatting inference.
+
+    Fuzzy matching uses an edit distance metric (Levenshtein distance by default)
+    to determine if a string comparison is a match.  If either the input token or
+    the tranlation (output) candidate is of length-1, the algorithm requires an
+    exact (case-insensitive) match.  All string comparison is case-insensitive by
+    default.
+
+    In addition to the kwarg parameters accepted by dict, SubstitutionMap
+    accepts the following kwarg params:
+
+    edit_fn : function or callable object
+        function to calculate edit distance.  By default, Levenshtein
+        distance is used.
+
+    dist : int or float
+        Maximum edit distance for fuzzy string matching.  The exact type
+        for this parameter will depend on the edit_fn being used.  The
+        default edit_fn uses an int.
+
+        Defaults to 0 (exact match)
+
+    case_sensitive : bool
+        If True, string comparisons are case-sensitive.
+        Defaults to False.
     """
-    Use:
-    ====
-    levenshtein_dist offers an intuitive metric for the "closeness"
-    of two strings.  It is useful for catching permutation of target strings
-    that differ in such aspects as capitalization and misspellings.
+    def __init__(self, *args, **kwargs):
+        # edit distance function
+        self.edit_fn = kwargs.get('edit_fn') or strndist.distance
 
-    Note:  for single words, a distance of 2 or 3 will catch most permutations.
+        # max levenshtein distance
+        self.dist = kwargs.get('dist') or 0
 
+        # case sensitivity
+        self.case_sensitive = kwargs.get('case_sensitive') or False
 
-    Details:
-    ========
-    Calculate the Levenshtein edit-distance between two strings.
-    The edit distance is the number of characters that need to be
-    substituted, inserted, or deleted, to transform s1 into s2.  For
-    example, transforming "rain" to "shine" requires three steps,
-    consisting of two substitutions and one insertion:
-    "rain" -> "sain" -> "shin" -> "shine".  These operations could have
-    been done in other orders, but at least three steps are needed.
+        self.update(*args, **kwargs)
+        self._convert_to_unicode()
 
-    :param s1, s2: The strings to be analysed
-    :type s1: str
-    :type s2: str
-    :rtype int
-    """
-    # set up a 2-D array
-    len1 = len(s1)
-    len2 = len(s2)
-    lev = _edit_dist_init(len1 + 1, len2 + 1)
+    def __call__(self, tokens):
+        return self.translate(tokens)
 
-    # iterate over the array
-    for i in range(len1):
-        for j in range(len2):
-            _edit_dist_step(lev, i + 1, j + 1, s1[i], s2[j])
+    def _convert_to_unicode(self):
+        for key in self:
+            value = self.pop(key)
 
-    return lev[len1][len2]
+            if isinstance(key, str):
+                key = unicode(key)
+            if isinstance(value, str):
+                value = unicode(value)
 
+            self[key] = value
 
-def _edit_dist_init(len1, len2):
-    """from NLTK 2.0
-    """
-    lev = []
-    for i in range(len1):
-        lev.append([0] * len2)  # initialize 2-D array to zero
-    for i in range(len1):
-        lev[i][0] = i  # column 0: 0,1,2,3,4,...
-    for j in range(len2):
-        lev[0][j] = j  # row 0: 0,1,2,3,4,...
-    return lev
+    def translate(self, tokens):
+        """Translate tokens based on contents of the SubstitutionMap.
 
+        tokens : sequence
+            Sequence of tokens to be matched and translated.
+        """
+        out_tokens = []
+        for token in tokens:
+            for orig, replace in self.iteritems():
+                if self.match(orig, token):
+                    token = fuzzyCaps(replace, token)
+                    break  # avoid re-translating the modified token
 
-def _edit_dist_step(lev, i, j, c1, c2):
-    """from NLTK 2.0
-    """
-    a = lev[i - 1][j] + 1  # skipping s1[i]
-    b = lev[i - 1][j - 1] + (c1 != c2)  # matching s1[i] with s2[j]
-    c = lev[i][j - 1] + 1  # skipping s2[j]
-    lev[i][j] = min(a, b, c)  # pick the cheapest
+            out_tokens.append(token)
+
+        return ''.join(out_tokens)
+
+    @normalize_unicode
+    def match(self, s1, s2):
+        # use exact matching for length-1 tokens (both original and replacement)
+        if len(s1) == 1 or len(s2) == 1:
+            dist = 0
+        else:
+            dist = self.dist
+
+        if not self.case_sensitive:
+            s1, s2 = s1.lower(), s2.lower()
+
+        return self.edit_fn(s1, s2) <= dist
