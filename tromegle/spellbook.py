77c59112de9169ad45bf3461c8c3b45a51184c03
Louis Thibault <louist@ltpc.(none)>
2013-03-13 11:39:41 +0100

    Minor tweaks

--- tromegle/spellbook.py
+++ tromegle/spellbook.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
-import tromegle.language
-from tromegle.event import spell
+import tromegle.language as language
+from tromegle.event import spell_
 
 
-ts_dict = {'m': 'f', 'male': 'female', 'guy': 'girl',
-           'f': 'm', 'female': 'male', 'girl': 'guy'}
-ts_map = tromegle.language.SubstitutionMap(ts_dict, dist=1)
-ts_stop = set(["I'm", "i'm"])
-ts_tokenizer = tromegle.language.Tokenizer(stop_words=ts_stop)
+ts_dict = {'m': 'f', 'male': 'female', 'guy': 'girl'}
+ts_map = language.SubstitutionMap(ts_dict, dist=1)
+ts_stop = set(["I'm"])
+ts_tokenizer = language.Tokenizer(stop_words=ts_stop)
 
 
-@spell
-def sex_change(t, ev, levdist=2):
+@spell_
+def sex_change(t, ev):
     if t.isMessage(ev):
         new_msg = ts_map(ts_tokenizer(ev.data))
         if t.msg_contents_modified(new_msg, ev.data):
