34342b72a8f7718b67bf10970b754dba63c834f7
Louis Thibault <louist@ltpc.(none)>
2013-03-13 06:54:28 +0100

    Updated sex_change spell

--- tromegle/spellbook.py
+++ tromegle/spellbook.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 import tromegle.language
-from tromegle.event import spell, Transmogrifier
+from tromegle.event import spell
 
 
-mfdict = {'m': 'f',
-           'male': 'female',
-           'guy': 'girl'}
+ts_dict = {'m': 'f', 'male': 'female', 'guy': 'girl',
+           'f': 'm', 'female': 'male', 'girl': 'guy'}
+ts_map = tromegle.language.SubstitutionMap(ts_dict, dist=1)
+ts_stop = set(["I'm", "i'm"])
+ts_tokenizer = tromegle.language.Tokenizer(stop_words=ts_stop)
 
 
 @spell
 def sex_change(t, ev, levdist=2):
-    if Transmogrifier.isMessage(ev):
-        msg = tromegle.language.tokenize(ev.data)
-
-# TODO:  levenshtein distance
-        # iterate over words, flipping all matches
-        new_msg = map(tromegle.language.fuzzyCaps,
-                      (mfdict.get(token, token) for token in msg),
-                      msg)
-
-        ev = Transmogrifier.modifyMessage(ev, new_msg)
+    if t.isMessage(ev):
+        new_msg = ts_map(ts_tokenizer(ev.data))
+        if t.msg_contents_modified(new_msg, ev.data):
+            ev = t.modifyMessage(ev, new_msg)
 
     return ev
